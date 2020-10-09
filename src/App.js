2ece4eca2b85a36e162b18801d51a2d17c618a59
Dirian Powell <dlpowell35@gmail.com>
2020-10-10 01:50:01 +0800

    need to update router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Homepage from "./Homepage";
+import Homepage from "./pages/homepage/Homepage.jsx";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./menu-item.scss";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl})` }}
+    className={`${size} menu-item`}
+    onClick={() => {
+      console.log(`${match.url}${linkUrl}`);
+      history.push(`${match.url}${linkUrl}`);
+    }}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem);
