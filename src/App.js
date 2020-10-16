9422fb92623e3d6e32d956a6cd61d2da6423bffd
Dirian Powell <dlpowell35@gmail.com>
2020-10-16 16:33:19 +0800

    added firebase in library

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import Homepage from "./pages/homepage/Homepage.jsx";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
+import Header from "./components/header/header";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
